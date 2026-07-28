7d941787779a9d98514cb20d4fadafe8acccccdc
lenovo <lenovo@DESKTOP-JC53660>
2026-07-28 11:00:04 +0300

    Add project files.

---
+++ Timer/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mtbHours = new System.Windows.Forms.MaskedTextBox();
+            this.mtbMinutes = new System.Windows.Forms.MaskedTextBox();
+            this.mtbSeconds = new System.Windows.Forms.MaskedTextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnContinue = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStart.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStart.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStart.ImageIndex = 2;
+            this.btnStart.ImageList = this.imageList1;
+            this.btnStart.Location = new System.Drawing.Point(77, 310);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(126, 46);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "pause.png");
+            this.imageList1.Images.SetKeyName(1, "play (1).png");
+            this.imageList1.Images.SetKeyName(2, "play.png");
+            this.imageList1.Images.SetKeyName(3, "refresh-arrow.png");
+            this.imageList1.Images.SetKeyName(4, "hourglass.png");
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(209, -17);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(328, 116);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Timer";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Timer.Properties.Resources.stop_watch;
+            this.pictureBox1.Location = new System.Drawing.Point(245, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 77);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AllowDrop = true;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(103, 172);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(75, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Hours";
+            // 
+            // label4
+            // 
+            this.label4.AllowDrop = true;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(280, 172);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label4.Size = new System.Drawing.Size(95, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Minutes";
+            // 
+            // label5
+            // 
+            this.label5.AllowDrop = true;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(477, 172);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label5.Size = new System.Drawing.Size(100, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Seconds";
+            // 
+            // mtbHours
+            // 
+            this.mtbHours.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbHours.Location = new System.Drawing.Point(108, 204);
+            this.mtbHours.Mask = "00";
+            this.mtbHours.Name = "mtbHours";
+            this.mtbHours.Size = new System.Drawing.Size(95, 85);
+            this.mtbHours.TabIndex = 10;
+            this.mtbHours.Text = "00";
+            this.mtbHours.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
+            // 
+            // mtbMinutes
+            // 
+            this.mtbMinutes.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbMinutes.Location = new System.Drawing.Point(287, 208);
+            this.mtbMinutes.Mask = "00";
+            this.mtbMinutes.Name = "mtbMinutes";
+            this.mtbMinutes.Size = new System.Drawing.Size(95, 85);
+            this.mtbMinutes.TabIndex = 11;
+            this.mtbMinutes.Text = "00";
+            this.mtbMinutes.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
+            // 
+            // mtbSeconds
+            // 
+            this.mtbSeconds.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbSeconds.Location = new System.Drawing.Point(482, 208);
+            this.mtbSeconds.Mask = "00";
+            this.mtbSeconds.Name = "mtbSeconds";
+            this.mtbSeconds.Size = new System.Drawing.Size(95, 85);
+            this.mtbSeconds.TabIndex = 12;
+            this.mtbSeconds.Text = "00";
+            this.mtbSeconds.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStop.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.White;
+            this.btnStop.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStop.ImageKey = "pause.png";
+            this.btnStop.ImageList = this.imageList1;
+            this.btnStop.Location = new System.Drawing.Point(535, 311);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(126, 46);
+            this.btnStop.TabIndex = 13;
+            this.btnStop.Text = "Stop";
+            this.btnStop.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.ImageIndex = 3;
+            this.button1.ImageList = this.imageList1;
+            this.button1.Location = new System.Drawing.Point(383, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 46);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Reset";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Rest_Clickt);
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnContinue.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnContinue.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContinue.ForeColor = System.Drawing.Color.White;
+            this.btnContinue.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnContinue.ImageIndex = 1;
+            this.btnContinue.ImageList = this.imageList1;
+            this.btnContinue.Location = new System.Drawing.Point(229, 312);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(126, 46);
+            this.btnContinue.TabIndex = 15;
+            this.btnContinue.Text = "Continue";
+            this.btnContinue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnContinue.UseVisualStyleBackColor = false;
+            this.btnContinue.Click += new System.EventHandler(this.btnContinue_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(773, 380);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnContinue);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.mtbSeconds);
+            this.Controls.Add(this.mtbMinutes);
+            this.Controls.Add(this.mtbHours);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = " Timer App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox mtbHours;
+        private System.Windows.Forms.MaskedTextBox mtbMinutes;
+        private System.Windows.Forms.MaskedTextBox mtbSeconds;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnContinue;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Timer/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        private short Hours;
+        private short Minutes;
+        private short Seconds;
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+        private void UpDateToTimer()
+        {
+            char Prefix = '0';
+
+            if(Hours <= 9)
+                mtbHours.Text = Prefix + Hours.ToString();
+            else
+                mtbHours.Text =  Hours.ToString();
+
+            if (Minutes <= 9)
+                mtbMinutes.Text = Prefix + Minutes.ToString();
+            else
+                mtbMinutes.Text = Minutes.ToString();
+
+            if (Seconds <= 9)
+                mtbSeconds.Text = Prefix + Seconds.ToString();
+            else
+                mtbSeconds.Text = Seconds.ToString();
+
+         }
+        private void SetTimer()
+        {
+            if (Hours > 0)
+            {
+                if (Minutes == 0)
+                {
+                    --Hours;
+                    Minutes = 59;
+                    Seconds = 60;
+                }
+
+            }
+            if (Minutes > 0)
+            {
+                if (Seconds == 0)
+                {
+                    --Minutes;
+                    Seconds = 60;
+                }
+
+
+            }
+            if (Hours == 0 && Minutes == 0 && Seconds == 0)
+            {
+                timer1.Enabled = false;
+                return;
+            }
+            --Seconds;
+            UpDateToTimer();
+        }
+        private void GetInput()
+        {
+            Hours = Convert.ToInt16(mtbHours.Text);
+            Minutes = Convert.ToInt16(mtbMinutes.Text);
+            Seconds = Convert.ToInt16(mtbSeconds.Text);
+        }
+        private void Reset()
+        {
+            timer1.Enabled = false;
+
+            mtbHours.Text = "00";
+            mtbMinutes.Text = "00";
+            mtbSeconds.Text = "00";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SetTimer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            GetInput();
+            timer1.Enabled = true;
+        }
+
+        private void Rest_Clickt(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+    }
+
